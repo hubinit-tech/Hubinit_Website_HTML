6e3c6cf0cbc529a0cad3f91a109eff24d274b915
GasparBonari <gasparbonari@gmail.com>
2023-12-02 18:23:30 +0100

    working on autoplay video

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
 // VIDEO LOGIC
 
-// Get the video element
+// Get the video element and overlay element
 let video = document.getElementById("myVideo");
+let overlay = document.getElementById("overlay");
+
+// Function to update overlay visibility and text when video is paused
+function updateOverlay() {
+  if (video.paused) {
+    overlay.style.display = "block";
+  } else {
+    overlay.style.display = "none";
+  }
+}
 
 // Function to play the video when it reaches the top of the screen
 function playVideoIfVisible() {
-    let videoOffset = video.getBoundingClientRect().top;
-    let windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  let videoOffset = video.getBoundingClientRect().top;
+  let windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
-    if (videoOffset < windowHeight) {
-        video.play();
-        // Optionally, you can remove the event listener once the video starts playing
-        window.removeEventListener("scroll", playVideoIfVisible);
-    }
+  console.log("Video Offset:", videoOffset);
+  console.log("Window Height:", windowHeight);
+
+  if (videoOffset <= 0) {
+    video.play();
+    window.removeEventListener("scroll", playVideoIfVisible);
+  }
 }
 
-// Add event listener to check for video visibility on scroll
+// Function to resume the video
+function resumeVideo() {
+  video.play();
+}
+
+// Add event listeners
 window.addEventListener("scroll", playVideoIfVisible);
+video.addEventListener("play", updateOverlay);
+video.addEventListener("pause", updateOverlay);
 
 // Optionally, you can also trigger the check on page load
 playVideoIfVisible();
